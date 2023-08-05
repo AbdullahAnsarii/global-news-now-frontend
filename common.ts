56ffedf3709c58184633a983a9f360e799c005c6
Abdullah Ansari <aa9272137@gmail.com>
2023-08-06 02:14:42 +0400

    update cyclic server link to heroku

--- common.ts
+++ common.ts
@@ -1,4 +1,4 @@
 export const FACEBOOK_URL = "https://www.facebook.com/";
 export const INSTAGRAM_URL = "https://www.instagram.com/";
 export const TWITTER_URL = "https://www.twitter.com/";
-export const BASE_URL_SERVER = "https://globalnewsnow.cyclic.app/";
+export const BASE_URL_SERVER = "https://global-news-now-58aafccd8d52.herokuapp.com/";
